47fa207848d043ce58a2c0c11dfe327f76cee9c3
VanshSh <vanshsharma9354@gmail.com>
2023-06-15 23:12:31 +0530

    feat: :sparkles: the phone book part 2.6 - 2.8

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "123-456-789" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [number, setNumber] = useState("");
+
+  // Add Name, Number Handler
+  const addNameHandler = (event) => {
+    event.preventDefault();
+    const newNameObj = {
+      name: newName,
+      number: number,
+    };
+
+    // Check if the name is already in the phonebook
+    const stringifyPersons = JSON.stringify(persons);
+    const stringifyNewNameObj = JSON.stringify(newNameObj);
+    if (stringifyPersons.includes(stringifyNewNameObj)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons([...persons, newNameObj]);
+    }
+    setNewName("");
+    setNumber("");
+  };
+
+  // Name Change Handler
+  const nameChangeHandler = (event) => {
+    setNewName(event.target.value);
+  };
+
+  // Number Change Handler
+  const numberChangeHandler = (event) => {
+    setNumber(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNameHandler}>
+        <div>
+          name: <input value={newName} onChange={nameChangeHandler} />
+        </div>
+        <div>
+          number: <input value={number} onChange={numberChangeHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
