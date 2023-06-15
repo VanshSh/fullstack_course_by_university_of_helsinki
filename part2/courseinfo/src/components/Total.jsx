9fb018b98548ea1f48baf4e19c4a4c98cabb7f2f
VanshSh <vanshsharma9354@gmail.com>
2023-06-15 22:28:52 +0530

    feat: :tada: start the phonebook part 2.6 -2.10

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -4,8 +4,6 @@ const Total = ({ course }) => {
   const totalExercises = course.parts.reduce((sum, part) => {
     return sum + part.exercises;
   }, 0);
-
   return <h3>total of {totalExercises}</h3>;
 };
-
 export default Total;
